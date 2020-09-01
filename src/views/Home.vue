f848a0436f60abcf96a2b2935a5188de58dae6f1
NISHAPURSWANI <43604816+NISHAPURSWANI@users.noreply.github.com>
2020-09-01 13:16:14 +0530

    new changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,49 +1,73 @@
 <template>
   <form id="jq-form" v-on:submit.prevent="submit" class="myform">
-    <div v-show="question_1" class="myform">
+    <div v-show="currentQuestion===1" class="myform">
       <label>
         <b>Enter your Name</b>
         <span class="text-danger">*</span>
       </label>
       <br />
       <input
-        type="text" 
+        type="text"
         v-model.trim="$v.fullname.$model"
         :class="{'is-invalid':validationStatus($v.fullname)}"
-        class="input" placeholder="Enter your name here "
-        
-        @keyup.enter="some()"
+        class="input"
+        placeholder="Enter your name here "
+        @keypress.enter="currentQuestion=currentQuestion+1"
       />
       <div v-if="!$v.fullname.required" class="invalid-feedback">The name field is required</div>
-      <br >press enter for next question
+      <br />press enter for next question
     </div>
     <br />
 
-   
-   <div v-show="question_2" class="myform"  @keypress="somec">
-        <div class="col-12 form-group">
-  <label class="col-form-label col-form-label-lg"><b>Enter your favorite color?</b> <span class="text-danger">*</span></label>
-<select v-model="$v.colors.$model" :class="{'is-invalid':validationStatus($v.colors)}" class="form-control form-control-lg">
-<option :value="c.name" :key="c.name" v-for="c in colorsList">{{c.colors}}</option>
-</select>
-      <br >press enter for next question
-
-      
-
+    <div
+      v-show="currentQuestion===2"
+      class="myform"
+      @keypress.enter="currentQuestion=currentQuestion+1"
+    >
+      <div class="col-12 form-group">
+        <label class="col-form-label col-form-label-lg">
+          <b>Enter your favorite color?</b>
+          <span class="text-danger">*</span>
+        </label>
+        <select
+          v-model="$v.colors.$model"
+          :class="{'is-invalid':validationStatus($v.colors)}"
+          class="form-control form-control-lg"
+        >
+          <option :value="c.name" :key="c.name" v-for="c in colorsList">{{c.colors}}</option>
+        </select>
+        <br />
+press enter for next question
       </div>
     </div>
 
-    <div v-show="question_3" class="myform">
+    <div v-show="currentQuestion===3" class="myform">
       <b>Q3. Do you have a pet?</b>
-      <br >
-     <input type="radio" id="yes" value="yes" v-model="ticked" @keyup.enter="somepy()" />
+      <br />
+      <input
+        type="radio"
+        id="yes"
+        value="yes"
+        v-model="ticked"
+        @keypress.enter="currentQuestion=currentQuestion+1"
+      />
       <label for="yes">Yes</label>
-      <input type="radio" id="no" value="no" v-model="ticked" @keyup.enter="somepn()" />
+      <input
+        type="radio"
+        id="no"
+        value="no"
+        v-model="ticked"
+        @keypress.enter="currentQuestion=currentQuestion+2"
+      />
       <label for="no">No</label>
       <br />press enter for next question
     </div>
     <br />
-    <div v-show="question_4" @keyup.enter="somepnm()" class="myform">
+    <div
+      v-show="currentQuestion===4"
+      @keypress.enter="currentQuestion=currentQuestion+3"
+      class="myform"
+    >
       <label>
         <b>Enter your pet's Name</b>
         <span class="text-danger">*</span>
@@ -53,18 +77,25 @@
         type="text"
         v-model.trim="$v.petname.$model"
         :class="{'is-invalid':validationStatus($v.petname)}"
-        class="input" placeholder="Enter your pet's here "
+        class="input"
+        placeholder="Enter your pet's here "
         @keyup.enter="somepn()"
       />
       <div v-if="!$v.petname.required" class="invalid-feedback">The name field is required</div>
       <br />press enter for next question
     </div>
     <br />
-    <div v-show="question_5" @keyup.enter="somes()" class="myform">
+    <div
+      v-show="currentQuestion===5"
+      @keypress.enter="currentQuestion=currentQuestion+1"
+      class="myform"
+    >
       <b>Q4. What are your Favorite Sports:</b>
-      <br >
-      <b-button  ><input type="checkbox" id="cricket" value="cricket" v-model="checkedNames" />
-      <label for="cricket">Cricket</label></b-button>
+      <br />
+      <b-button>
+        <input type="checkbox" id="cricket" value="cricket" v-model="checkedNames" />
+        <label for="cricket">Cricket</label>
+      </b-button>
       <input type="checkbox" id="football" value="football" v-model="checkedNames" />
       <label for="football">Football</label>
       <input type="checkbox" id="badminton" value="badminton" v-model="checkedNames" />
@@ -73,14 +104,13 @@
       <label for="tennis">Tennis</label>
       <input type="checkbox" id="baseball" value="baseball" v-model="checkedNames" />
       <label for="baseball">BaseBall</label>
-      <br />
-press enter for next question
+      <br />press enter for next question
       <br />
     </div>
-    <div v-show="question_6" class="myans">
+    <div v-show="currentQuestion===6" class="myans">
       <img alt="Vue logo" src="../assets/thankyou.png" style="width:300px;height:300px;" />
       <br />
-<br>
+      <br />
       <span>My name is {{fullname}}</span>
       <br />
       <span class="opcolor">My favorite color is {{colors}}</span>
@@ -90,10 +120,10 @@ press enter for next question
       <span>My favorite sports are: {{ checkedNames }}</span>
       <br />
     </div>
-    <div v-show="question_7" class="myans">
+    <div v-show="currentQuestion===7" class="myans">
       <img alt="Vue logo" src="../assets/thankyou.png" style="width:300px;height:300px;" />
       <br />
-<br>
+      <br />
       <span>My name is {{fullname}}</span>
       <br />
       <span style="color:val">My favorite color is {{colors}}</span>
@@ -108,7 +138,7 @@ press enter for next question
 </template>
 <script>
 import { required } from "vuelidate/lib/validators";
-let colors = require('../data')
+let colors = require("../data");
 
 export default {
   name: "jqForm",
@@ -118,25 +148,17 @@ export default {
       petname: "",
       ticked: "",
       checkedNames: [],
-      colorsList:colors,
+      colorsList: colors,
       selected: "",
-      question_1: true,
-      question_2: false,
-      question_3: false,
-      question_4: false,
-      question_5: false,
-      question_6: false,
-      question_7: false
+      currentQuestion: 1
     };
   },
-  
+
   validations: {
     fullname: { required },
     petname: { required },
     colors: { required }
   },
-
-  
 
   methods: {
     validationStatus: function(validation) {
@@ -145,26 +167,9 @@ export default {
     submit: function() {
       this.$v.$touch();
       if (this.$v.$pendding || this.$v.$error) return;
-    },
-
-    some() {
-      (this.question_2 = true), (this.question_1 = false), (this.question_3 = false);
-    },
-    somec() {
-      (this.question_3 = true), (this.question_2 = false);
-    },
-    somepy() {
-      (this.question_4 = true), (this.question_3 = false);
-    },
-    somepn() {
-      (this.question_5 = true), (this.question_3 = false);
-    },
-    somepnm() {
-      (this.question_7 = true), (this.question_5 = false), (this.question_4 = false);
-    },
-    somes() {
-      (this.question_6 = true), (this.question_5 = false), (this.question_3 = false);
     }
+
+    
   }
 };
 </script>
@@ -173,17 +178,17 @@ export default {
 .myform {
   margin-top: 100px;
   margin-left: -400px;
-  font-family: 'Arvo';
+  font-family: "Arvo";
 
   font-size: 18px;
 }
-.input{
+.input {
   background-color: transparent;
   border: none;
   border-bottom: 1px solid blue;
   color: rgb(0, 0, 0);
   box-sizing: border-box red;
-  font-family: 'Arvo';
+  font-family: "Arvo";
   font-size: 18px;
   height: 30px;
   padding: 10px 0px;
@@ -194,7 +199,4 @@ export default {
   margin-top: 10px;
   margin-left: -350px;
 }
-
-
-
 </style>
